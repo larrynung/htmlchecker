fea812a40fa488ff3a9c805a472602e20db4e49a
larrynung <larrynung@larrynungs-MacBook-Pro.local>
2018-07-10 01:20:24 +0800

    Use transform stram & output stream

--- test/development-requirement.test.js
+++ test/development-requirement.test.js
@@ -5,72 +5,74 @@ const expect = require('chai').expect;
 const path = require('path');
 const HeadTagShouldHaveMetaTagRule = require('../lib/Rules/HeadTagShouldHaveMetaTagRule');
 const Rule = require('../lib/Rule');
-const {Checker, InputType, OutputType} = require('../lib/checker');
+const { Checker, InputType, OutputType } = require('../lib/checker');
 const checker = new Checker();
 
-describe('Development Requirement', function(){
-    it('User is free to chain any rules by themselves', function(){
+describe('Development Requirement', function () {
+    it('User is free to chain any rules by themselves', function () {
         checker.registeredRules.ImgTagShouldHaveAltAttributeRule.isActive = false;
-        expect(
-            checker
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-            .check()
-        ).to.be.equal(true);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img alt='value'/>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
         checker.registeredRules.ImgTagShouldHaveAltAttributeRule.isActive = true;
-        expect(
-            checker
+
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-            .check()
-        ).to.be.equal(false);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img alt='value'/>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('User can define and use their own rules easily', function(){
-        let aTagShouldHaveHrefAttributeRule = new Rule('ATagShouldHaveHrefAttributeRule', 'a:not([href])', function(length) { return 'There are ' + length + ' <a> tag without href attribute.'; });
-        expect(
-            checker
+    it('User can define and use their own rules easily', function () {
+        let aTagShouldHaveHrefAttributeRule = new Rule('ATagShouldHaveHrefAttributeRule', 'a:not([href])', function (length) { return 'There are ' + length + ' <a> tag without href attribute.'; });
+
+        checker
             .input(InputType.Text, "<a href=''/>")
             .rules([aTagShouldHaveHrefAttributeRule])
-            .check()
-        ).to.be.equal(true);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
+        checker
             .input(InputType.Text, "<a/>")
             .rules([aTagShouldHaveHrefAttributeRule])
-            .check()
-        ).to.be.equal(false);
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
     });
-    it('The input can be a HTML file (User is able to config the output destination)', function(){
+    it('The input can be a HTML file (User is able to config the output destination)', function () {
         let file = path.join(__dirname, 'input', 'input.html');
-        expect(
-            checker
+        checker
             .input(InputType.File, file)
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
     it('The input can be Node Readable Stream');
-    it('The output can be a file (User is able to config the output destination)', function(){
+    it('The output can be a file (User is able to config the output destination)', function () {
         let file = path.join(__dirname, 'output', 'output.txt');
         if (fs.existsSync(file)) {
             fs.unlinkSync(file);
         }
         checker
-        .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-        .output(OutputType.File, file)
-        .check();
+            .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
+            .output(OutputType.File, file)
+            .check();
         expect(fs.existsSync(file)).to.be.equal(true);
     });
-    it('The output can be Node Writable Stream', function(){
+    it('The output can be Node Writable Stream', function () {
         let folderPath = path.join(__dirname, 'output');
-        if (!fs.existsSync(folderPath)){
+        if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath);
         }
         let file = path.join(folderPath, 'output.txt');
@@ -79,30 +81,33 @@ describe('Development Requirement', function(){
         }
         let stream = fs.createWriteStream(file);
         checker
-        .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-        .output(OutputType.Stream, stream)
-        .check();
+            .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
+            .output(OutputType.Stream, stream)
+            .check();
         expect(fs.existsSync(file)).to.be.equal(true);
     });
-    it('The output can be Console', function(){
+    it('The output can be Console', function () {
         checker
-        .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-        .output(OutputType.Console)
-        .check();
+            .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
+            .output(OutputType.Console)
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('When we want to implement additional rules for <meta> tag, The code changes should be small. Ex: Checking <meta name=“robots” /> existing or not?!', function(){
+    it('When we want to implement additional rules for <meta> tag, The code changes should be small. Ex: Checking <meta name=“robots” /> existing or not?!', function () {
         let headTagShouldHaveMetaTagWithRobotsRule = new HeadTagShouldHaveMetaTagRule('HeadTagShouldHaveMetaTagWithRobotsRule', 'robots', 'This HTML without <meta name="robots" .../> tag.');
-        expect(
-            checker
+
+        checker
             .input(InputType.Text, "<head><meta name='robots'/></head>")
             .rules([headTagShouldHaveMetaTagWithRobotsRule])
-            .check()
-        ).to.be.equal(true);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
+        checker
             .input(InputType.Text, "<head></head>")
             .rules([headTagShouldHaveMetaTagWithRobotsRule])
-            .check()
-        ).to.be.equal(false);
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
     });
 });
--- test/predefined-seo-rules.test.js
+++ test/predefined-seo-rules.test.js
@@ -1,103 +1,110 @@
 'use strict';
 
-const expect = require('chai').expect;
-const {Checker, InputType} = require('../lib/checker');
+const chai = require('chai');
+const expect = chai.expect;
+const { Checker, InputType } = require('../lib/checker');
 const checker = new Checker();
 
-describe('Pre-defined SEO rules', function(){
-    it('Detect if any <img/> tag without alt attribute', function(){
-        expect(
-            checker
+describe('Pre-defined SEO rules', function () {
+    it('Detect if any <img/> tag without alt attribute', function () {
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img/>")
-            .check()
-        ).to.be.equal(false);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+    });
+    it('Detect if any <img/> tag without alt attribute', function () {
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><img alt='value'/>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if any <a/> tag without rel attribute', function(){
-        expect(
-            checker
+    it('Detect if any <a/> tag without rel attribute', function () {
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><a/>")
-            .check()
-        ).to.be.equal(false);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><a rel='value'/>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if header doesn’t have <title> tag', function(){
-        expect(
-            checker
+    it('Detect if header doesn’t have <title> tag', function () {
+        checker
             .input(InputType.Text, "<head><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(false);        
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if header doesn’t have <meta name=”descriptions” ... /> tag', function(){
-        expect(
-            checker
+    it('Detect if header doesn’t have <meta name=”descriptions” ... /> tag', function () {
+
+        checker
             .input(InputType.Text, "<head><title></title><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(false);     
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if header doesn’t have <meta name=“keywords” ... />', function(){
-        expect(
-            checker
+    it('Detect if header doesn’t have <meta name=“keywords” ... />', function () {
+
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/></head>")
-            .check()
-        ).to.be.equal(false);   
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(true);
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if there’re more than 15 <strong> tag in HTML (15 is a value should be configurable by user)', function(){
+    it('Detect if there’re more than 15 <strong> tag in HTML (15 is a value should be configurable by user)', function () {
         checker.registeredRules.MoreThanNStrongTagRule.params.maxStrongTagCount = 15;
-        expect(
-            checker
+
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong>")
-            .check()
-        ).to.be.equal(false); 
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong><strong></strong>")
-            .check()
-        ).to.be.equal(true);             
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(true);        
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
-    it('Detect if a HTML have more than one <H1> tag', function(){
-        expect(
-            checker
+    it('Detect if a HTML have more than one <H1> tag', function () {
+
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><H1></H1><H1></H1>")
-            .check()
-        ).to.be.equal(false);   
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(false);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head><H1></H1>")
-            .check()
-        ).to.be.equal(true);
-        expect(
-            checker
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
+        checker
             .input(InputType.Text, "<head><title></title><meta name='descriptions'/><meta name='keywords'/></head>")
-            .check()
-        ).to.be.equal(true);        
+            .check(function (result) {
+                expect(result).to.be.equal(true);
+            });
     });
 });
